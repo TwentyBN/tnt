8c595c38362829bde2029be505a8ad35286707b5
Ingo F <ingo.fruend@twentybn.com>
2016-11-28 14:02:16 +0100

    adding summary

--- setup.py
+++ setup.py
@@ -71,6 +71,7 @@ if __name__ == '__main__':
     setup(
         name='tnt',
         version='0.11.0.2',
+        summary='tnt is not tensorflow',
         classifiers=[
             'Development Status :: 4 - Beta',
             'Intended Audience :: Developers',
