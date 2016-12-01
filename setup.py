3cd4c5a15d7c90978925e1af95c04d4af6454e9f
Ingo F <ingo.fruend@twentybn.com>
2016-12-01 15:05:46 +0100

    tf0.12.0rc0 is on pypi

--- setup.py
+++ setup.py
@@ -1,77 +1,24 @@
-import sys
 import os
 from distutils.core import setup
 import pip
 
-TFVERSION = "0.12.0"
-
-
-class UnsupportedPlatformException(Exception):
-    pass
-
 
 def install_tensorflow():
-    python_version = pythonversion()
-    has_gpu = hasgpu()
-    platform_name = platform()
-    install_url = build_url(TFVERSION,
-                            python_version,
-                            "gpu" if has_gpu else "cpu",
-                            platform_name,
-                            )
-    pip.main(['install', '-U', install_url])
-
-
-def pythonversion():
-    return sys.version_info[:2]
+    install_target = 'tensorflow'
+    if hasgpu():
+        install_target += '-gpu'
+    pip.main(['install', '-U', install_target])
 
 
 def hasgpu():
     return not os.system('which nvidia-smi > /dev/null')
-
-
-def platform():
-    platform_name = sys.platform
-    if platform_name.startswith('linux'):
-        return 'linux'
-    elif platform_name.startswith('darwin'):
-        return 'mac'
-    else:
-        raise UnsupportedPlatformException(
-            'Only linux and mac are supported, but you are on "{}"'
-            .format(platform_name))
-
-
-def create_wheeltags(platform_name, python_version):
-    if platform_name == 'mac':
-        return 'py{}-none-any'.format(python_version[0])
-    elif platform_name == 'linux':
-        cpstring = '{}{}'.format(python_version[0], python_version[1])
-        if cpstring == '27':
-            return 'cp27-none-linux_x86_64'
-        else:
-            return 'cp{}-cp{}m-linux_x86_64'.format(cpstring, cpstring)
-
-
-def build_url(tf_version, python_version, processing_unit, platform_name):
-    kwargs = {
-        'platform_name': platform_name,
-        'processing_unit': processing_unit,
-        'tf_version': tf_version,
-        'wheeltags': create_wheeltags(platform_name, python_version)
-    }
-    return ('https://storage.googleapis.com/tensorflow/'
-            '{platform_name}/{processing_unit}/'
-            'tensorflow-{tf_version}-{wheeltags}.whl'
-            .format(**kwargs))
-
 
 if __name__ == '__main__':
     install_tensorflow()
 
     setup(
         name='tnt',
-        version='0.12.0.6',
+        version='0.12.0.7',
         description='tnt is not tensorflow',
         classifiers=[
             'Development Status :: 4 - Beta',
--- test.py
+++ test.py
@@ -4,11 +4,6 @@ import shutil
 import stat
 import unittest
 
-try:
-    import unittest.mock as mock
-except ImportError:
-    import mock
-
 import setup
 
 
@@ -40,64 +35,3 @@ class TestHasGPU(unittest.TestCase):
     def test_has_gpu_fails(self):
         os.environ['PATH'] = remove_nvidia_smi_from_path(os.environ['PATH'])
         self.assertFalse(setup.hasgpu())
-
-
-class TestPlatform(unittest.TestCase):
-
-    @mock.patch('sys.platform', 'linux2')
-    def test_linux_detected_correctly(self):
-        self.assertEqual(setup.platform(), 'linux')
-
-    @mock.patch('sys.platform', 'darwin')
-    def test_darwin_dected_correctly(self):
-        self.assertEqual(setup.platform(), 'mac')
-
-    @mock.patch('sys.platform', 'windows')
-    def test_unsupported_platform_exception_raised_on_windows(self):
-        self.assertRaises(setup.UnsupportedPlatformException, setup.platform)
-
-
-class TestWheeltags(unittest.TestCase):
-
-    def test_mac_27(self):
-        wheeltags = setup.create_wheeltags('mac', (2, 7))
-        self.assertEqual(wheeltags, 'py2-none-any')
-
-    def test_mac_34(self):
-        wheeltags = setup.create_wheeltags('mac', (3, 4))
-        self.assertEqual(wheeltags, 'py3-none-any')
-
-    def test_mac_35(self):
-        wheeltags = setup.create_wheeltags('mac', (3, 5))
-        self.assertEqual(wheeltags, 'py3-none-any')
-
-    def test_linux_27(self):
-        wheeltags = setup.create_wheeltags('linux', (2, 7))
-        self.assertEqual(wheeltags, 'cp27-none-linux_x86_64')
-
-    def test_linux_34(self):
-        wheeltags = setup.create_wheeltags('linux', (3, 4))
-        self.assertEqual(wheeltags, 'cp34-cp34m-linux_x86_64')
-
-    def test_linux_35(self):
-        wheeltags = setup.create_wheeltags('linux', (3, 5))
-        self.assertEqual(wheeltags, 'cp35-cp35m-linux_x86_64')
-
-
-class TestBuildURL(unittest.TestCase):
-
-    @mock.patch("setup.create_wheeltags")
-    def test_build_url_assembles_correctly(self, wheeltags_mock):
-        wheeltags_mock.return_value = 'ANY_WHEELTAGS'
-        expected = ("https://storage.googleapis.com/tensorflow/"
-                    "ANY_PLATFORM_NAME/ANY_PROCESSING_UNIT/"
-                    "tensorflow-ANY_TF_VERSION-ANY_WHEELTAGS.whl")
-        result = setup.build_url(
-            "ANY_TF_VERSION",
-            "ANY_PYTHON_VERSION",
-            "ANY_PROCESSING_UNIT",
-            "ANY_PLATFORM_NAME",
-        )
-        self.assertEqual(result, expected)
-        wheeltags_mock.assert_called_once_with("ANY_PLATFORM_NAME",
-                                               "ANY_PYTHON_VERSION")
